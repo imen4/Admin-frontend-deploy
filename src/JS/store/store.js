925c5b743e93ee1307e8b1154e1aca3a40a14417
imen <imenjlassi02@gmail.com>
2023-02-26 10:29:08 +0100

    deploy

--- src/JS/store/store.js
+++ src/JS/store/store.js
@@ -1,12 +1,8 @@
 import {createStore , applyMiddleware , compose} from "redux";
 import thunk from "redux-thunk";
 import {rootReducer} from "../reducers/index"
-import { nodeEnv } from '~/utils/config';
 
-const composeEnhancers = (nodeEnv !== 'production' &&
-typeof window !== 'undefined' &&
-window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-compose;
+const composeEnhancers =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
     rootReducer,
